10eab36ef6cb021c24a8f1ead48f5db392740512
Craig <c.matear@gmail.com>
2025-08-16 19:26:59 +0000

    feat: Adding tests
    * Added a test for embedding cache

---
+++ mealie-auto-tagger/tests/caches/test_embeddingCache.py
@@ -0,0 +1,27 @@
+import pytest
+from mealie_auto_tagger.caches.embeddingCache import __EmbeddingCache
+from mealie_auto_tagger.model.mealie.shoppingListItem import MealieLabel
+
+@pytest.fixture
+def new_cache():
+    return __EmbeddingCache()
+
+@pytest.fixture
+def test_label1():
+    return MealieLabel(name="test_label1", color="blue", groupId="123",id="124")
+
+@pytest.fixture
+def test_label2():
+    return MealieLabel(name="test_label2", color="blue", groupId="123",id="124")
+
+def test_cache_creates(new_cache: __EmbeddingCache, test_label1: MealieLabel, test_label2: MealieLabel):
+    embedding1 = new_cache.getLabelEmbedding(test_label1)
+    embedding2 = new_cache.getLabelEmbedding(test_label2)
+    assert embedding1 != None
+    assert embedding2 != None
+    assert embedding1 != embedding2
+
+def test_cache_caches(new_cache: __EmbeddingCache, test_label1: MealieLabel):
+    embedding1 = new_cache.getLabelEmbedding(test_label1)
+    embedding2 = new_cache.getLabelEmbedding(test_label1)
+    assert embedding2 == embedding1
